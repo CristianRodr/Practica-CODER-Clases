1fcdba83ce766eb51be2881742132a4d125fe37f
cristianRodri <an1me@hotmail.es>
2024-02-06 00:38:33 -0500

    comentarios readme actualizado

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ class ProductManeger {
 
   addProduct( title = '', description = '', price = 0, thumbnail = '', code = 0, stock = '') {
 
+    //id generado automáticamente SIN REPETIRSE
     let id=1
     const n = this.products.length;
     if(n>0){
@@ -17,15 +18,30 @@ class ProductManeger {
 
     const product = {id, title, description, price, thumbnail, code, stock}
 
+    const arr = [product]
 
+    //Campos Obligatorios------
+    for (const arrElement of arr) {
+      for (const arrElementKey in arrElement) {
+        if (arrElement[arrElementKey] === '' || arrElement[arrElementKey] === 0) {
+          console.log(`componente <<${arrElementKey}>> no ingresado, completar componente`);
+          console.log('--------------------------------------------------------------')
+          return;
+        }
+      }
+    }
+
+    //error porque CODE estará repetido----
     let codeExiste = this.products.find(codeVal => codeVal.code === code );
     if (!codeExiste) {
       return this.products.push(product);
     } else {
-      console.log(`'Producto con <<code ${code}>> existente no admitido -> cambia code`);
+      console.log(`'Producto <<code ${code}>> existente no admitido -> cambia code`);
+      console.log('----------------------------------------------------------------')
     }
   }
 
+  //metodo buscar ID
   getProductByID(id) {
     const byId = this.products.find(busId => busId.id === id)
     const found = byId === undefined ? "Not found" : [byId];
@@ -35,17 +51,27 @@ class ProductManeger {
 
 // Instancia Objeto------------------------------------------------------------
 const product = new ProductManeger();
-//=============================================================================
+//======retorno array [] vació===========
+console.log(product.getProducts());
+//======método “addProduct” con los campos===================================
 product.addProduct( 'producto prueba0','Este es un producto prueba1',
     200, 'Sin Imagen', 'abc123', 20);
-product.addProduct('producto prueba1', 'Este es un producto prueba2',
+
+product.addProduct('producto prueba0','Este es un producto prueba2',
     200, 'Sin Imagen', 'abc124', 30);
+
 product.addProduct('producto prueba2', 'Este es un producto prueba3',
     200, 'Sin Imagen', 'abc125', 40);
+
+//arrojar un error porque el código estará repetido---------------
 product.addProduct('producto prueba3', 'Este es un producto prueba4',
     200, 'Sin Imagen', 'abc124', 50);
-//=============================================================================
-console.log(product.getProducts());
-//=============================================================================
-product.getProductByID(2);
-product.getProductByID(6);
+
+//arrojar un error porque falta componente------------------------
+product.addProduct( 'Este es un producto prueba4',
+    200, 'Sin Imagen', 'abc126', 50);
+//==========================================================================
+console.log(product.getProducts()); //debe aparecer el producto recién agregado
+//===========================================================================
+product.getProductByID(2); //buscar arreglo con id conincidente
+product.getProductByID(6); //id no coincide arroja error
